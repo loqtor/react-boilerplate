9b1c695016414e01b832392be9cee44227a62c50
Nathaniel Watson <nathaniel@alphero.com>
2019-03-19 09:30:22 +1300

    Remove icons that are not Alphero’s

--- client/src/tools/utilities/icomoon-content.js
+++ client/src/tools/utilities/icomoon-content.js
@@ -3,30 +3,6 @@
 // The script that generates this file is located at tools/icomoon-generator.js
 // To update this file, selection.json must be changed and
 // the script run 'yarn run generate-icons'
-import React from 'react';
 
 export default {
-  'preview': <span className="icon icon-preview" />,
-  'check': <span className="icon icon-check" />,
-  'add': <span className="icon icon-add" />,
-  'search': <span className="icon icon-search" />,
-  'dropdown': <span className="icon icon-dropdown" />,
-  'chevron-down': <span className="icon icon-chevron-down" />,
-  'chevron-up': <span className="icon icon-chevron-up" />,
-  'chevron-left': <span className="icon icon-chevron-left" />,
-  'chevron-right': <span className="icon icon-chevron-right" />,
-  'arrow-back': <span className="icon icon-arrow-back" />,
-  '10-back': <span className="icon icon-10-back"><span className="path1" key="1" /><span className="path2" key="2" /><span className="path3" key="3" /></span>,
-  'play': <span className="icon icon-play" />,
-  'player-play': <span className="icon icon-player-play" />,
-  'player-pause': <span className="icon icon-player-pause"><span className="path1" key="1" /><span className="path2" key="2" /></span>,
-  '10-forward': <span className="icon icon-10-forward"><span className="path1" key="1" /><span className="path2" key="2" /><span className="path3" key="3" /></span>,
-  'captions': <span className="icon icon-captions" />,
-  'captions-active': <span className="icon icon-captions-active"><span className="path1" key="1" /><span className="path2" key="2" /></span>,
-  'episodes': <span className="icon icon-episodes"><span className="path1" key="1" /><span className="path2" key="2" /></span>,
-  'full-screen-enter': <span className="icon icon-full-screen-enter" />,
-  'full-screen-exit': <span className="icon icon-full-screen-exit" />,
-  'skip': <span className="icon icon-skip" />,
-  'mute': <span className="icon icon-mute" />,
-  'volume': <span className="icon icon-volume" />,
 };
